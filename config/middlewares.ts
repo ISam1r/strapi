3dfe0189a43400a8020b8d20318641193a9605b5
Samir Isakov <samirisakov64@gmail.com>
2025-03-01 13:47:26 +0500

    remove cors settings

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,19 +1,12 @@
 export default [
-  "strapi::errors",
-  "strapi::security",
-  {
-    name: "strapi::cors",
-    config: {
-      enabled: true,
-      origin: ["https://strapi-gt2u.onrender.com",], // Разрешённые домены
-      methods: ["GET", "POST", "PUT", "DELETE"], // Разрешённые методы
-      headers: ["Content-Type", "Authorization"], // Разрешённые заголовки
-      credentials: true, // Если нужно передавать cookies
-    },
-  },
-  "strapi::query",
-  "strapi::body",
-  "strapi::session",
-  "strapi::favicon",
-  "strapi::public",
+  'strapi::logger',
+  'strapi::errors',
+  'strapi::security',
+  'strapi::cors',
+  'strapi::poweredBy',
+  'strapi::query',
+  'strapi::body',
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
 ];
